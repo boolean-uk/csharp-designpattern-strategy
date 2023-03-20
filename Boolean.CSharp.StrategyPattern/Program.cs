06375d355145979708c68ceb1f80a782ff83eadd
uerbzr <nigel.sibbert@gmail.com>
2023-03-20 09:47:06 +0000

    Add project files.

---
+++ Boolean.CSharp.StrategyPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Boolean.CSharp.StrategyPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Strategy Pattern Example");
+            
+            TravelPlanner travelPlanner = new TravelPlanner();
+
+       
+
+
+            travelPlanner.SetTravelStrategy(new Plane());
+            travelPlanner.Drive(1100);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Boolean.CSharp.StrategyPattern/TravelStrategy/Plane.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean.CSharp.StrategyPattern
+{
+    public class Plane : TravelStrategy
+    {
+        public Plane()
+        {
+            KilometerCost = 150;
+        }
+
+        public override decimal Drive(int kilometers)
+        {
+            if (kilometers > 1000)
+            {
+                KilometerCost = 15;
+            }
+
+            return kilometers * KilometerCost;
+        }
+    }
+}
